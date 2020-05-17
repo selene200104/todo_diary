4d09d269190d54131dacc3740baa35eba89a32c2
Juhee Jang <selene170402@naver.com>
2020-05-17 20:54:53 +0900

    로그아웃 기능 구현

--- app/src/main/java/com/example/todo_diary/MainActivity.java
+++ app/src/main/java/com/example/todo_diary/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.todo_diary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +19,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton logoutButton = (ImageButton)findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //SharedPreferences에 저장된 값들을 로그아웃 버튼을 누르면 삭제하기 위해
+                //로그인 액티비티에서 만든 이름으로 SharedPreferences를 불러옴
+                Intent intent = new Intent(MainActivity.this, loginActivity.class);
+                startActivity(intent);
+                SharedPreferences login = getSharedPreferences("auto", AppCompatActivity.MODE_PRIVATE);
+                SharedPreferences.Editor editor = login.edit();
+                //editor.clear()는 auto에 들어있는 모든 정보를 기기에서 지움
+                editor.clear();
+                editor.commit();
+                Toast.makeText(MainActivity.this, "로그아웃하였습니다", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
 
         //달력이미지를 누르면 스케쥴관리 액티비티로 이동
         ImageButton goScheduleButton = (ImageButton)findViewById(R.id.goScheduleButton);
