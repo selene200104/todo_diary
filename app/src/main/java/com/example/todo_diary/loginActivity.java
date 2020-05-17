8bf37d0f2fca289426fa1f5ff1a2326743f7b626
Juhee Jang <selene170402@naver.com>
2020-05-17 20:24:07 +0900

    로그인시 미리 저장되어있던 로그인 관련 데이터(아이디,비밀번호)와 같으면 로그인이 되면서 넘어감

--- app/src/main/java/com/example/todo_diary/loginActivity.java
+++ app/src/main/java/com/example/todo_diary/loginActivity.java
@@ -3,17 +3,57 @@ package com.example.todo_diary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class loginActivity extends AppCompatActivity {
+
+    EditText id, password;
+    Button loginButton;
+    String loginId, loginPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        id = (EditText)findViewById(R.id.emailAddress);
+        password = (EditText)findViewById(R.id.password);
+        loginButton = (Button)findViewById(R.id.loginButton);
+
+        SharedPreferences login = getSharedPreferences("auto", AppCompatActivity.MODE_PRIVATE);
+
+        loginId = login.getString("inputId",null);
+        loginPassword = login.getString("inputPassword",null);
+
+        if(loginId == null && loginPassword == null) {
+            loginButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //아이디와 비밀번호에 입력된 값이 미리 지정되어있는 값과 같으면 로그인이 된다.
+                    if (id.getText().toString().equals("juhee@naver.com") && password.getText().toString().equals("951215")) {
+                        SharedPreferences login = getSharedPreferences("auto", AppCompatActivity.MODE_PRIVATE);
+                        //아이디가 'juhee'이고 비밀번호가 '951215'일 경우 SharedPreferences.Editor를 통해
+                        //auto의 loginId와 loginPwd에 값을 저장해 줍니다.
+                        SharedPreferences.Editor autoLogin = login.edit();
+                        autoLogin.putString("inputId", id.getText().toString());
+                        autoLogin.putString("inputPwd", password.getText().toString());
+                        //commit()을 통해 값 저장(하지 않으면 저장이 되지 않음)
+                        autoLogin.commit();
+                        //Toast를 통해 환영메세지를 띄움
+                        Toast.makeText(loginActivity.this, id.getText().toString() + "님 환영합니다.", Toast.LENGTH_SHORT).show();
+                        //로그인 액티비티에서 메인 액티비티로 넘어감
+                        Intent intent = new Intent(loginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+            });
+        }
 
         //등록버튼을 누르면 회원가입 액티비티로 이동
         Button goRegisterButton = (Button)findViewById(R.id.goRegisterButton);
@@ -27,7 +67,7 @@ public class loginActivity extends AppCompatActivity {
             }
         });
 
-        //로그인을 누르면 앱 메인 액티비티로 이동
+        /*//로그인을 누르면 앱 메인 액티비티로 이동
         Button loginButton = (Button)findViewById(R.id.loginButton);
         loginButton.setOnClickListener(new View.OnClickListener() {
 
@@ -37,7 +77,7 @@ public class loginActivity extends AppCompatActivity {
                 Intent intent = new Intent(loginActivity.this, MainActivity.class);
                 startActivity(intent);
             }
-        });
+        }); */
     }
 /*
     @Override
