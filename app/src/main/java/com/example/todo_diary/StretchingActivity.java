1c9238de45a457f4f661284dfaf00f460da4cdc4
Juhee Jang <selene170402@naver.com>
2020-05-25 14:21:39 +0900

    스트레칭 타임 이미지 배열명 변경

--- app/src/main/java/com/example/todo_diary/StretchingActivity.java
+++ app/src/main/java/com/example/todo_diary/StretchingActivity.java
@@ -6,20 +6,23 @@ import android.os.Bundle;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+
 public class StretchingActivity extends AppCompatActivity {
 
     int stretchingTimeMax = 6;
-    int[] imgs = new int[stretchingTimeMax];
+    int[] stretchingTimeImgs = new int[stretchingTimeMax];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stretching);
 
+        //스트레칭 타임 이미지를 배열에 넣어줌
         for (int i = 0; i < stretchingTimeMax; i++) {
-            imgs[i] = getApplicationContext().getResources().getIdentifier( "minute"+i, "drawable", "com.example.todo_diary");
+            stretchingTimeImgs[i] = getApplicationContext().getResources().getIdentifier( "minute"+i, "drawable", "com.example.todo_diary");
         }
         ImageView minuteImage = (ImageView) findViewById(R.id.timeImage);
-        minuteImage.setImageResource(imgs[stretchingTimeMax-1]);
+        minuteImage.setImageResource(stretchingTimeImgs[stretchingTimeMax-1]);
     }
 }
